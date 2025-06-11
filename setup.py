48c62eca729f87a728a40e11d94c219d55d69758
Abhishek <dummy1cx@gmail.com>
2025-06-11 18:17:51 +0100

    initial setup for project, venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhishek Das",
+    author_email="dummy1cx@gmail.com",
+    packages=find_packages()
+)
